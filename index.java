18dafc260ea1faccd191346e365ebe5479983c3d
Roger <rogerolivares30@gmail.com>
2021-04-22 17:00:21 +0200

    actualizat codi fibbo

--- index.java
+++ index.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
 class Fibonacci {
     public static void main(String[] args) {
         int n, a = 0, b = 0, c = 1;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter value of n:");
-        n = s.nextInt();
+        n = Integer.parseInt(args[0]);
         System.out.print("Fibonacci Series:");
         for(int i = 1; i <= n; i++)
         {
